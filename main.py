3c6a7d3d4bdde6b6a7f5df947ea5b56f1be6af1a
codingaTool <eghosaosula20@gmail.com>
2024-05-12 03:00:46 -0400

    add dealer hand sequence

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-from cards import hearts, black_jack_cards
+from cards import hearts, diamond, spades, clubs,black_jack_cards
+import random
 
+
+print("Welcome to Black Jack" +' '+ hearts +' ' + diamond +' '+ spades +' '+ clubs)
+print("Enter 'S' to Start ")
+
+init_dealer_hand = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+final_dealer_hand = random.sample(init_dealer_hand, 2)
+
+print(final_dealer_hand)
