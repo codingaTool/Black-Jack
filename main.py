7eae1de94f96b9b855acad6af1e538a4fd6a3f1a
codingaTool <eghosaosula20@gmail.com>
2024-05-13 18:21:04 -0400

    cleanup hand_dealt_gen function, optimised code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from cards import hearts, diamond, spades, clubs, card_dictionary
 import random
 
-#dealer_hand =[]
-#user_hand = []
 
 print("Welcome to Black Jack" +' '+ hearts +' ' + diamond +' '+ spades +' '+ clubs)
 print("Enter 'S' to Start ")
@@ -22,29 +20,13 @@ def hand_dealt_gen (init_hand_dealt_dictionary):
     user_hand = random.choices (init_hand_dealt_dictionary, k=2)
     return user_hand
 
+#assign 2 list items to user and computer at start of game
 user_hand_dealt = hand_dealt_gen (init_hand_dealt_dictionary=initial_hand_dealt)
-print("user dealt", user_hand_dealt)
+print("user dealt -->", user_hand_dealt)
 dealer_hand_dealt = hand_dealt_gen (init_hand_dealt_dictionary=initial_hand_dealt)
-print("dealer dealt", dealer_hand_dealt)
+print("dealer dealt -->", dealer_hand_dealt[0] +','+"[card hidden]")
 
 #deal = input('Enter deal, hit or stand')
 
-''' 
 
-def gameplay (hand_dealt,hand):
 
-    if hand == 'hit':
-        new_user_hand = user_hand.append(random.choice(initial_hand_dealt))
-        print(new_user_hand)
-        #computer_hand = random. choice (hand_dealt)
-
-        #return computer_hand
-        #print(computer_hand)
-    else:
-        hand == 'stand'
-        computer_hand = random.choices (hand_dealt,k=3)
-        print(computer_hand)
-
-gameplay(hand_dealt=initial_hand_dealt, hand=deal)
-
-'''
